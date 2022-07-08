94cdf0b98442579e098efb0b9e6f493bb62d52b7
Taisei-Tsuno <tsunon.0508@gmail.com>
2022-07-08 14:48:31 +0900

    HoT:initial revision

---
+++ random.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+int main()
+{
+  srand((unsigned int)time(NULL));
+  int a[3],i;
+  int flag=0;
+  printf("Tossing a coin...\n");
+for(i=0;i<3;i++)
+{
+   a[i] = rand();
+   if(a[i]%2==0)
+   {
+     flag++;
+     printf("Round %d: Heads\n",i+1);
+   }
+   else{
+     printf("Round %d: Tails\n",i+1);
+   }
+}
+printf("Heads: %d, Tails: %d\n",flag,3-flag);
+return 0;
+}
